4f122042ed58bbd757256a2586864986b67f17ad
ilantc42 <version.ilan@gmail.com>
2022-01-01 12:32:41 +0200

    Swithched from dataclass to attr

--- cfi_midot/items.py
+++ cfi_midot/items.py
@@ -4,32 +4,32 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 from typing import Optional
-from dataclasses import dataclass, field
+import attr
 
 
-@dataclass(frozen=True)
+@attr.s(frozen=True, auto_attribs=True)
 class NgoTopRecipientSalary:
     recipient_title: str
     gross_salary_in_nis: float
 
 
-@dataclass(frozen=True)
+@attr.s(frozen=True, auto_attribs=True)
 class NgoTopRecipientsSalaries:
     top_earners_salaries: list[NgoTopRecipientSalary]
     report_year: int
 
 
-@dataclass(frozen=True)
+@attr.s(frozen=True, auto_attribs=True)
 class NgoGeneralInfo:
     ngo_name: str
     ngo_goal: str
     ngo_year_founded: int
-    volunteers_num: Optional[int] = field(default=None)
-    employees_num: Optional[int] = field(default=None)
-    ngo_members_num: Optional[int] = field(default=None)
+    volunteers_num: Optional[int] = attr.ib(default=None)
+    employees_num: Optional[int] = attr.ib(default=None)
+    ngo_members_num: Optional[int] = attr.ib(default=None)
 
 
-@dataclass
+@attr.s(frozen=True, auto_attribs=True)
 class NgoFinanceInfo:
     report_year: int
 
@@ -51,24 +51,34 @@ class NgoFinanceInfo:
     other_income_from_other_sources: int
     other_income_members_fee: int
 
-    expenses_salary_for_activities: int = field(default=0)
-    other_expenses_for_activities: int = field(default=0)
-    donations_value_for_money: int = field(default=0)
+    expenses_salary_for_activities: int = attr.ib(default=0)
+    other_expenses_for_activities: int = attr.ib(default=0)
+    donations_value_for_money: int = attr.ib(default=0)
 
-    @property
-    def total_allocations(self) -> int:
+    # ------------ Computed ------------
+    total_allocations: int = attr.ib(init=False)
+    total_donations: int = attr.ib(init=False)
+    total_expenses: int = attr.ib(init=False)
+    total_service_income: int = attr.ib(init=False)
+    total_other_income: int = attr.ib(init=False)
+    # ------------ Ratios ------------
+    program_expense_ratio: Optional[float] = attr.ib(init=False)
+    administrative_expense_ratio: Optional[float] = attr.ib(init=False)
+
+    @total_allocations.default
+    def _total_allocations(self) -> int:
         return (
             self.allocations_from_government
             + self.allocations_from_local_authority
             + self.allocations_from_other_sources
         )
 
-    @property
-    def total_donations(self) -> int:
+    @total_donations.default
+    def _total_donations(self) -> int:
         return self.donations_from_aboard + self.donations_from_israel
 
-    @property
-    def total_expenses(self) -> int:
+    @total_expenses.default
+    def _total_expenses(self) -> int:
         return (
             self.expenses_other
             + self.other_expenses_for_activities
@@ -77,21 +87,21 @@ class NgoFinanceInfo:
             + self.expenses_salary_for_activities
         )
 
-    @property
-    def total_service_income(self) -> int:
+    @total_service_income.default
+    def _total_service_income(self) -> int:
         return (
             self.service_income_from_country
             + self.service_income_from_local_authority
             + self.service_income_from_other
         )
 
-    @property
-    def total_other_income(self) -> int:
+    @total_other_income.default
+    def _total_other_income(self) -> int:
         return self.other_income_from_other_sources + self.other_income_members_fee
 
     # ------------ Ratios ------------
-    @property
-    def program_expense_ratio(self) -> Optional[float]:
+    @program_expense_ratio.default
+    def _program_expense_ratio(self) -> Optional[float]:
 
         total_program_expenses = (
             self.other_expenses_for_activities + self.expenses_salary_for_activities
@@ -102,8 +112,8 @@ class NgoFinanceInfo:
 
         return (total_program_expenses / self.total_expenses) * 100
 
-    @property
-    def administrative_expense_ratio(self) -> Optional[float]:
+    @administrative_expense_ratio.default
+    def _administrative_expense_ratio(self) -> Optional[float]:
 
         total_administrative_expenses = (
             self.expenses_salary_For_management
@@ -117,13 +127,13 @@ class NgoFinanceInfo:
         return (total_administrative_expenses / self.total_expenses) * 100
 
 
-@dataclass(frozen=True)
+@attr.s(frozen=True, auto_attribs=True)
 class NgoInfo:
     ngo_id: int
 
     general_info: NgoGeneralInfo
-    financial_info: Optional[NgoFinanceInfo] = field(default=None)
-    top_earners_info: Optional[NgoTopRecipientsSalaries] = field(default=None)
+    financial_info: Optional[NgoFinanceInfo] = attr.ib(default=None)
+    top_earners_info: Optional[NgoTopRecipientsSalaries] = attr.ib(default=None)
 
     @classmethod
     def from_resource_items(cls, ngo_id: int, resources_items: dict) -> "NgoInfo":
